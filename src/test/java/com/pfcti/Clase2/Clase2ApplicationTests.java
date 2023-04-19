57a40a26b957b5d3f496c77801bbe38c744e58b6
Jose Ramirez <jose.ramirez@grupopromerica.com>
2023-04-18 18:06:14 -0600

    Primer commit Clase2
    Primer commit Clase2

---
+++ src/test/java/com/pfcti/Clase2/Clase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.Clase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
